cafc7ae5aab067dc1805b5324f04ce6792aa3da3
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-17 22:07:51 +0100

    Other CURD funcs with hotel route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -24,6 +24,33 @@ router.route('/:id').put(async(req,res)=>{
    }
 })
 //delete
+router.route('/:id').delete(async(req,res)=>{
+   try{
+      const deletedHotel=await Hotel.findByIdAndDelete(req.params.id);
+      res.status(200).json({success: true,hotel:deletedHotel});
+   }
+   catch(err){
+      res.status(500).json({success:false,err});
+   }
+})
 //get
+router.route('/:id').get(async(req,res)=>{
+   try{
+      const hotel=await Hotel.findById(req.params.id);
+      res.status(200).json({success: true,hotel:hotel});
+   }
+   catch(err){
+      res.status(500).json({success:false,err});
+   }
+})
 //getall
+router.route('/').get(async(req,res)=>{
+   try{
+      const hotels=await Hotel.findById();
+      res.status(200).json({success: true,hotel:hotels});
+   }
+   catch(err){
+      res.status(500).json({success:false,err});
+   }
+})
 export default router;
