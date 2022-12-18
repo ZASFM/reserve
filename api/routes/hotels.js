51c3639c2fee0c6b8de636b170abfefda46ee152
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-12-18 20:56:58 +0100

    JWT+Cookies verification with routes

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router=express.Router();
 
-//create
-router.route('/').post(createHotel);
+//create, only admin can create, delete and update, so before i have to check that im admin
+router.route('/').post(verifyAdmin,createHotel);
 //update
-router.route('/:id').put(updateHotel)
+router.route('/:id').put(verifyAdmin,updateHotel)
 //delete
-router.route('/:id').delete(deleteHotel)
+router.route('/:id').delete(verifyAdmin,deleteHotel)
 //get
 router.route('/:id').get(getHotel)
 //getall
--- api/routes/users.js
+++ api/routes/users.js
@@ -14,13 +14,13 @@ router.route('/checkuser/:id').get(verifyUser,(req,res,next)=>{
 router.route('/checkadmin/:id').get(verifyAdmin,(req,res,next)=>{
    res.send('Authenticated admin, you are admin');
 }) */
-//update
-router.route('/:id').put(updateUser)
+//update, you have to be a user to be able to update delete or get your account
+router.route('/:id').put(verifyUser,updateUser)
 //delete
-router.route('/:id').delete(deleteUser)
+router.route('/:id').delete(verifyUser,deleteUser)
 //get
-router.route('/:id').get(getUser)
-//getall
-router.route('/').get(getUsers)
+router.route('/:id').get(verifyUser,getUser)
+//getall, only admins can get all the users
+router.route('/').get(verifyAdmin,getUsers)
 
 export default router;
